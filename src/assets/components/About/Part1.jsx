f5ddddfb47690b42435b8bcb46ea33ed2ddec3ce
Vanshika Indoria <162411918+VanshikaSI@users.noreply.github.com>
2026-04-21 07:57:21 +0530

    Update Part1.jsx

--- src/assets/components/About/Part1.jsx
+++ src/assets/components/About/Part1.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
 import { motion } from "framer-motion";
-// Import your single preferred image here
 import aboutHero from "../../images/1.jpeg";
 
 const Part1 = () => {
